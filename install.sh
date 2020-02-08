d8beae72c6346e550b5365e3712ef4ec2cb2a17f
shawn226 <35102149+shawn226@users.noreply.github.com>
2020-02-08 19:48:46 +0100

    tryc

--- install.sh
+++ install.sh
@@ -4,9 +4,20 @@ echo "Bienvenue dans le quide d'installation d'Arch Linux.\n"
 echo "Veuillez suivre les instructions à l'écran."
 
 what_kind_of_boot()
-{ (
-    if [[ test -e /sys/firmware/efi/efivars -a  test -d /sys/firmware/efi/efivars ]]
+{ 
+    if [[ -d "/sys/firmware/efi/efivars" ]]
     then
-        echo "efi"
+        return 1
+	else
+		return 0
     fi
-) }
+}
+
+boot=what_kind_of_boot
+
+if [ boot -eq 1 ]
+then
+	echo "C'est parti pour l'efi !"
+else
+	echo "ah non c'est du boot"
+fi
