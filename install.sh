f3971ebd99de053adaca7a256351db25f01a400c
shawn226 <35102149+shawn226@users.noreply.github.com>
2020-02-09 13:53:50 +0100

    lol

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 echo "Bienvenue dans le quide d'installation d'Arch Linux.\n"
 echo "Veuillez suivre les instructions à l'écran."
 
+###FONCTIONS
+
+#Fonction qui permet de récupérer le mode de boot
 what_kind_of_boot()
 { 
     if [[ -d "/sys/firmware/efi/efivars" ]]
@@ -13,12 +16,33 @@ what_kind_of_boot()
     fi
 }
 
-what_kind_of_boot
-efi = $?
+set_time_by_timezone(){
+	answer = "no"
+	
+	while [[ $answer -eq "no" ]]
+	do
+		#set the date set-ntp
+		timedatectl set-ntp true
 
-if [[ $efi -eq 1 ]]
-then
-	echo "C'est parti pour l'efi !"
-else
-	echo "ah non c'est du boot"
-fi
+		read -p "\nIndiquez votre continent : " continent
+		read -p "Indiquez votre ville : " city
+
+		timedatectl set-timezone $continent/$city
+		
+		timedatectl status
+		
+		read -p "\nEst-ce correct ? (yes / no): " answer
+	done
+}
+
+# define what is the boot => can change settings 
+what_kind_of_boot
+efi = $? # efi is set to 1 if it's true
+
+sleep(2)
+set_time_by_timezone
+
+
+
+
+
